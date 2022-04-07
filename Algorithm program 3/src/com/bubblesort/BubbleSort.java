b07aca43f6b114c70bb49c08b94688e4a1c6e539
swasthikkj <99305988+swasthikkj@users.noreply.github.com>
2022-04-07 17:12:49 +0530

    program

---
+++ Algorithm program 3/src/com/bubblesort/BubbleSort.java
@@ -0,0 +1,31 @@
+package com.bubblesort;
+
+public class BubbleSort {
+	public static void main(String[] args) {
+		int[] list = { 12, 78, 9, 2, 3, 45 };
+		BubbleSort.sortList(list);
+	}
+
+	private static void sortList(int[] list) {
+		int temp;
+		for (int i = 0; i < list.length; i++) {
+			int flag = 0;
+			for (int j = 0; j < list.length - 1 - i; j++) {
+				if (list[j] > list[j + 1]) {
+					temp = list[j];
+					list[j] = list[j+1];
+					list[j+1] = temp;
+					flag = 1;
+				}
+			}
+			
+			if(flag == 0) {
+				break;
+			}
+		}
+
+		for (int i = 0; i < list.length; i++) {
+			System.out.print(list[i] + " ");
+		}
+	}
+}
