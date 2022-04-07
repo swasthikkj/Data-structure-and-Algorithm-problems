4270ec58a1338a5221fd5f057cf09ed871edcbe7
swasthikkj <99305988+swasthikkj@users.noreply.github.com>
2022-04-07 17:12:23 +0530

    program

---
+++ Algorithm program 2/src/com/insertionsort/InsertionSort.java
@@ -0,0 +1,36 @@
+package com.insertionsort;
+
+public class InsertionSort {
+	public String[] sortArray(String array[], int n) {
+		for (int i = 1; i < array.length; i++) {
+			String temp = array[i];
+			int j = i - 1;
+			while (j >= 0 && temp.length() < array[j].length())
+			{
+				array[j+1] = array[j];
+				j--;
+			}
+			
+			array[j+1] = temp;
+		}
+		
+		for (int i = 0; i<array.length; i++) {
+			System.out.print(array[i]+" ");
+			return array;	
+		}
+		
+		return array;
+	}
+
+	public static void main(String[] args) {
+		InsertionSort sort = new InsertionSort();
+		String[] array = {"Swasthik","Pavan","Karthik","Surya"};
+		System.out.println("Before insertion sort");
+		for(int i = 0; i<array.length; i++) {
+			System.out.print(array[i]+" ");
+			int n = array.length;
+			System.out.println("\nAfter insertion sort");
+			sort.sortArray(array,n);
+		}
+	}
+}
