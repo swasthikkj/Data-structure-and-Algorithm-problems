f3888d72f5d70931f78a4960cc29c7f51a3ed0b5
swasthikkj <99305988+swasthikkj@users.noreply.github.com>
2022-04-07 17:14:46 +0530

    program

---
+++ Data structure 2/src/com/cashcounter/Person.java
@@ -0,0 +1,12 @@
+package com.cashcounter;
+
+public class Person {
+	Person nextPerson;
+	int money;
+	int bankBalance = 1000;
+	public Person( int money) {
+		this.nextPerson = null;
+		this.money = money;
+		CashCounter.noOfCustomers++;
+	}
+}
